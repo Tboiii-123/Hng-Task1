2b5eecc55967ee355f81b2a14abdbf52ffab045a
Lawal Hussein <132585505+Tboiii-123@users.noreply.github.com>
2025-02-04 13:26:18 +0100

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from  django.urls import path
 from . import views
 
 urlpatterns = [
-            path('api/',views.number_classification, name="number_classification")
+            path('api/classify-number',views.number_classification, name="number_classification")
     ]
 
     
